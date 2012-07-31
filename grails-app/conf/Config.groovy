158eb798b1ec0bf10a93ec56b2268e316f937fde
unknown <weihong0427@163.com>
2012-07-31 12:11:43 +0800

    change pulgin

--- grails-app/conf/Config.groovy
+++
@@ -1,6 +0,0 @@
-simian{
-    reportsDir="target/simian-reports"
-    fileDir="."
-    cludesFiles=[includes:"**/*.groovy **/*.java **/*.gsp **/*.js **/*.css",excludes:"**/bootstrap*.css"]
-    methodParams=[threshold:20]
-}
